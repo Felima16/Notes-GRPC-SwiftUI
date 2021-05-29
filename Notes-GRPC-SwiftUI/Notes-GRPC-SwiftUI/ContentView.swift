89666b24313d79a4937597ec2ea0beaf77b46c20
Fernanda de Lima <limade.f@gmail.com>
2021-05-29 08:19:55 -0300

    created project

---
+++ Notes-GRPC-SwiftUI/Notes-GRPC-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Notes-GRPC-SwiftUI
+//
+//  Created by Fernanda  de Lima on 29/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
